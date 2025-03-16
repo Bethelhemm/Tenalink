3964a0cf193e47551aa1631ff527e7d6d592c795
red095 <redietteklay8@gmail.com>
2025-03-16 20:24:10 +0300

    made some changes

--- models/user.js
+++ models/user.js
@@ -1,31 +1,31 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/config');
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config/config'); // Ensure this file exports a Sequelize instance
 
-const User= sequelize.define('User', {
-    id:{
-        type:DataTypes.UUID
-        ,defaultValue:DataTypes.UUIDV4
-        ,primaryKey:true
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
     },
-    email:{
-        type:DataTypes.STRING
-        ,allowNull:false
-        ,unique:true
-
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
     },
-    password_hash:{
-        type:DataTypes.STRING
-        ,
+    password_hash: {
+        type: DataTypes.STRING
     },
-    role:{
-        type:DataTypes.ENUM('government','hospital','doctor'),
-        allowNull:false
+    role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            isIn: [['government', 'hospital', 'doctor']]
+        }
     },
-    is_active:{
-        type:DataTypes.BOOLEAN
-        ,defaultValue:true
+    is_active: {  // Moved outside of "role"
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
     }
-
 });
 
 module.exports = User;
